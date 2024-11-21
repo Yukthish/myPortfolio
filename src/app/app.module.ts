332c312d73d8eacfd01b21b95935307b1f5d097c
Yukthish <95063800+Yukthish@users.noreply.github.com>
2024-11-21 22:22:41 +0530

    2nd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,12 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProjectsComponent } from './components/projects/projects.component';
-import { ContactComponent } from './components/contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'skills', component: SkillsComponent },
   { path: 'projects', component: ProjectsComponent },
-  { path: 'contact', component: ContactComponent }
 ];
 
 @NgModule({
@@ -32,7 +30,6 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    ContactComponent,
     RouterModule.forRoot(routes),
     ReactiveFormsModule
     
